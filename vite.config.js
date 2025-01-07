3ee29902627230d38987dec7ba688099160245ab
Alejandro <eadelgadom@gmail.com>
2025-01-07 17:00:20 +0000

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+//base: "/shoppingreact",
 export default defineConfig({
-  base: "/shoppingreact",
+  base: "/",
   plugins: [react()],
 })
